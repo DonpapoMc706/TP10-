4f433e4f74e1d84c604f50bc82517cc5f55b0da4
DonpapoMc706 <pianarosa.zaid.et21.25@gmail.com>
2026-06-18 23:04:07 +0000

    Guardo esqueleto de mi_malloc para rellenar la proxima

--- ej1.c
+++ ej1.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+typedef struct {
+    int id;
+    char nickname[30];
+    int edad;
+    char pais[30];
+} usuario;
 
-typedef struct { [cite: 46]
-    void* dirrecion; [cite: 47]
-    size_t bytes; [cite: 48]
-    char archivo[100]; [cite: 49]
-} bloqueMemoria; [cite: 50]
+typedef struct {
+    void* direccion;
+    size_t bytes;
+    char archivo[100];
+    int línea;
+} BloqueMemoria;
 
-bloqueMemoria* crearBloqueMemoria(size_t bytes, const char* archivo) { [cite: 51]
-    bloqueMemoria* bloque = (bloqueMemoria*)malloc(sizeof(bloqueMemoria)); [cite: 52]
-    if (bloque == NULL) { [cite: 53]
-        return NULL; [cite: 54]
-    } [cite: 55]
-    bloque->dirrecion = malloc(bytes); [cite: 56]
-    if (bloque->dirrecion == NULL) { [cite: 57]
-        free(bloque); [cite: 58]
-        return NULL; [cite: 59]
-    } [cite: 60]
-    bloque->bytes = bytes; [cite: 61]
-    strncpy(bloque->archivo, archivo, sizeof(bloque->archivo) - 1); [cite: 62]
-    bloque->archivo[sizeof(bloque->archivo) - 1] = '\0'; [cite: 63]
-    return bloque; [cite: 64]
+BloqueMemoria registro[100];
+
+
+ malloc(size) mi_malloc(size, __FILE__, __LINE__)
+ free(ptr) mi_free(ptr, __FILE__, __LINE__)
+void* mi_malloc(size_t size, const char* archivo, int línea) {
+    void* ptr = malloc(size);
+    if (ptr == NULL) {
+        fprintf(stderr, "Error al asignar memoria en %s:%d\n", archivo, línea);
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
 }
