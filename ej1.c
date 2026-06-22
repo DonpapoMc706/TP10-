91ad3972d7a1e28430ca7ae353625e66f322f46f
DonpapoMc706 <pianarosa.zaid.et21.25@gmail.com>
2026-06-22 22:27:15 +0000

    Guardo avance de mi_malloc con la logica del registro

--- ej1.c
+++ ej1.c
@@ -16,15 +16,25 @@ typedef struct {
 } BloqueMemoria;
 
 BloqueMemoria registro[100];
+int totalBloques = 0;
 
-
- malloc(size) mi_malloc(size, __FILE__, __LINE__)
- free(ptr) mi_free(ptr, __FILE__, __LINE__)
-void* mi_malloc(size_t size, const char* archivo, int línea) {
-    void* ptr = malloc(size);
-    if (ptr == NULL) {
-        fprintf(stderr, "Error al asignar memoria en %s:%d\n", archivo, línea);
-        exit(EXIT_FAILURE);
-    }
+void *mi_malloc(size_t bytes, const char *archivo, int línea) {
+      void *ptr = malloc(bytes);
+        if (ptr != NULL) {
+            for (int i = 0; i < 100; i++) {
+                if (registro[i].direccion == NULL) {
+                    registro[i].direccion = ptr;
+                    registro[i].bytes = bytes;
+                    strncpy(registro[i].archivo, archivo, sizeof(registro[i].archivo) - 1);
+                    registro[i].archivo[sizeof(registro[i].archivo) - 1] = '\0';
+                    registro[i].línea = línea;
+                    totalBloques++;
+                    break;
+        
+                }
+            }
+        }
     return ptr;
-}
+    } 
+#define malloc(x) mi_malloc(x, __FILE__, __LINE__)
+#define free(x) mi_free(x, __FILE__, __LINE__)
