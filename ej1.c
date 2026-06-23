9ecfe1ca4f105b92bca2d533e3b031aa670bc412
DonpapoMc706 <pianarosa.zaid.et21.25@gmail.com>
2026-06-23 23:02:42 +0000

    Guardo avance de mi_malloc con la logica del registro

--- ej1.c
+++ ej1.c
@@ -20,9 +20,7 @@ int totalBloques = 0;
 
 void *mi_malloc(size_t bytes, const char *archivo, int línea) {
       void *ptr = malloc(bytes);
-         {
-            registro[totalBloques].direccion = ptr; {
-                if (registro[totalBloques].direccion == NULL) {
+                if (ptr != NULL) {
                     registro[totalBloques].direccion = ptr;
                     registro[totalBloques].bytes = bytes;
                     strncpy(registro[totalBloques].archivo, archivo, sizeof(registro[totalBloques].archivo) - 1);
@@ -30,10 +28,8 @@ void *mi_malloc(size_t bytes, const char *archivo, int línea) {
                     registro[totalBloques].línea = línea;
                     totalBloques++; 
                 }
-            }
-        }
-    return ptr;
-    } 
+                return ptr;
+            } 
     void mi_free(void* ptr, const char* archivo, int línea) {
         free(ptr);
         for (int i = 0; i < totalBloques; i++) {
@@ -42,10 +38,19 @@ void *mi_malloc(size_t bytes, const char *archivo, int línea) {
                     registro[j] = registro[j + 1];
                 }
                 totalBloques--;
-                break;
+               break;
             }
-    
-    
-}
+        }   
+    }
+    void mostrarMemoriaReservada() {
+        printf("=====BLOQUES ACTIVOS=====\n");
+        for (int i = 0; i < totalBloques; i++) {
+            printf("Direccín: %p, Bytes: %zu, Archivo: %s, Línea: %d\n",
+                     registro[i].direccion,
+                     registro[i].bytes,
+                     registro[i].archivo,
+                     registro[i].línea);
+        }
+    }
 #define malloc(x) mi_malloc(x, __FILE__, __LINE__)
 #define free(x) mi_free(x, __FILE__, __LINE__)
